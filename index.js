b3f0452894f22b7d201a102f58f484e282984052
tatomyr <andrew.tatomyr@gmail.com>
2017-01-21 16:39:52 +0200

    working prototype

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-// We have a set of operation duration intervals.
-// Find every possible combination of total duration
-// Assume that there are only two possible duration for each operation - min or max
+var T = [];
 
 const tableSubmit = () => {
+  const t = getTasksMinMax();
+  console.log(t);
+
+  const vector = new DiscreteVector(t);
+  T = Array(vector.combinations).fill().map((prev, item) => embodiment(t, vector.next())).sort((a, b) => a - b);
+
+  console.log(T);
+
+
+  const P = 0.80;
+  console.log(
+    `${P * 100}%: [${Math.ceil((T.length - 1) * P)}] =>`,
+    T.sort((a, b) => a - b)[Math.ceil((T.length - 1) * P)]
+  );
+
+  $('#container').css({ "display": "block" });
+  setChart(T);
+
+
+}
+
+const estimate = () => {
+  console.log($('#probability').val());
+  console.log(getPercentage());
+
+
+  const P = Number($('#probability').val()) / 100;
+  const t = T[Math.ceil((T.length - 1) * P)];
+  const result = t * getPercentage();
+  console.log(t, result);
+
+  $('#result').html(`RESULT: ${result} H`);
+
+
+}
+
+const getTasksMinMax = () => {
   const t = [];
   $('#table .task').each((i, task)=> {
     const min = $(task).find('.min').val();
@@ -12,26 +47,15 @@ const tableSubmit = () => {
       Number(max),
     ]);
   });
-  console.log(t);
+  return t;
+}
 
-
-
-
-  const J = new DiscreteVector(t);
-  const T = [];
-  for (let n = 0; n < J.combinations; n++) {
-    T.push(embodiment(t, J.next()));
-  }
-  console.log(T.sort((a, b) => a - b));
-  const P = 0.95;
-  console.log(
-    `${P * 100}%: [${Math.ceil((T.length - 1) * P)}] =>`,
-    T.sort((a, b) => a - b)[Math.ceil((T.length - 1) * P)]
-  );
-
-
-  setChart(T);
-
+const getPercentage = () => {
+  let percentage = 0;
+  $('#calculation .percentage').each((i, item)=> {
+    percentage += Number(item.value);
+  });
+  return 1 + percentage / 100;
 }
 
 const embodiment = (a, b) => a.reduce((prev, item, i) => prev + item[b[i]], 0);
@@ -80,4 +104,20 @@ const setChart = (T) => {
       }
     }
   });
+}
+
+const addTask = () => {
+  $('tbody').append(
+    `<tr class="task">
+      <td>
+        <input type="text" class="description" placeholder="Task..." />
+      </td>
+      <td>
+        <input type="number" class="min" />
+      </td>
+      <td>
+        <input type="number" class="max" />
+      </td>
+    </tr>`
+  );
 }
