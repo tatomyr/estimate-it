43fd1478b0bb8a2d715f10702cc67cf13eded551
tatomyr <andrew.tatomyr@gmail.com>
2018-07-09 23:48:51 +0300

    editor-config: even more improved configuration

--- src/components/Estimate/editor-config.js
+++ src/components/Estimate/editor-config.js
@@ -1,21 +1,30 @@
 /* eslint-disable quotes */
 
+// This config defines the editor's view.
 export const options = {
   lineNumbers: false,
   scrollBeyondLastLine: false,
   readOnly: false,
 }
 
+// This config defines how the language is displayed in the editor.
 export const languageDef = {
   defaultToken: "",
   number: /\d+(\.\d+)?/,
+  keywords: [
+    "@project",
+    "@developer",
+    "@pm",
+    "@summary",
+    "@rounding",
+  ],
   tokenizer: {
     root: [
       { include: "@whitespace" },
       { include: "@numbers" },
       { include: "@strings" },
-      [/^[@%][a-zA-Z]\w*/, "tag"],
-      [/#[a-zA-Z]\w*/, "tag"],
+      { include: "@tags" },
+      [/^@\w+/, { cases: { "@keywords": "keyword" } }],
     ],
     whitespace: [
       [/^\s*# .*$/, "comment"],
@@ -27,9 +36,14 @@ export const languageDef = {
     strings: [
       [/[=|][ @number]*$/, "string.escape"],
     ],
+    tags: [
+      [/^%[a-zA-Z]\w*/, "tag"],
+      [/#[a-zA-Z]\w*/, "tag"],
+    ],
   },
 }
 
+// This config defines the editor's behavior.
 export const configuration = {
   comments: {
     lineComment: "#",
