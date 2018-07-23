f376c1666c7171f3f4384fb9abf2b9f0b33fab11
tatomyr <andrew.tatomyr@gmail.com>
2018-07-23 15:37:14 +0300

    Updates

---
+++ static/js/5.54593d1a.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{728:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=728}});
+//# sourceMappingURL=5.54593d1a.chunk.js.map
