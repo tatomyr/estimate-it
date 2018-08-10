5fff6745113957cfcec31ee5b96891a14f07f35f
tatomyr <andrew.tatomyr@gmail.com>
2018-08-11 00:12:22 +0300

    getting monaco editor web worker loaded

--- public/monaco-editor-worker-loader-proxy.js
+++ public/monaco-editor-worker-loader-proxy.js
@@ -1,1 +1,1 @@
-self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.13.1/min/vs/base/worker/workerMain.js')
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.13.1/min/vs/base/worker/workerMain.js') // eslint-disable-line
