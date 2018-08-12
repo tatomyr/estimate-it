29f2736462423b925ab1052ef45b054ac2cdc095
tatomyr <andrew.tatomyr@gmail.com>
2018-08-12 19:17:01 +0300

    Updates

--- static/js/5.180642f1.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{734:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=734}});
-//# sourceMappingURL=5.180642f1.chunk.js.map
---
+++ static/js/5.37422ada.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{735:function(n,o){function e(n){throw new Error("Cannot find module '"+n+"'.")}e.keys=function(){return[]},e.resolve=e,n.exports=e,e.id=735}});
+//# sourceMappingURL=5.37422ada.chunk.js.map
